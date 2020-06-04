614e62596081393446fcbd02dd9300ac208f5f19
shaico <shaico92@gmail.com>
2020-06-04 22:52:02 +0300

    starting to add some icons for properties

---
+++ public/js/generalmethods.js
@@ -0,0 +1,30 @@
+var minPrice = document.getElementsByName('price');
+
+minPrice.minValue = 0.01
+
+
+var properties = document.getElementById('forModal');
+
+properties.addEventListener('click',function(){
+    
+})
+
+{/* <div class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Modal title</h5>  
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>Modal body text goes here.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div> */}
